3b58763ebcc0a2df35414c63fa1fd7e5d742152b
tarunmalik330 <maliktarun2005@gmail.com>
2023-09-09 14:12:56 +0530

    add-new-section

---
+++ assest/css/main.js
@@ -0,0 +1,12 @@
+//buttons and counter text//
+// const counter = document.querySelector('#counter')
+// const btn = document.querySelectorAll('.btn')
+
+// .btns.forEach((btn) => {
+//     btn.addEventsListener('click', (e) =>
+//     const styles = e.currentTarget classList
+//         if (styles .conytains ('increase')) {
+            
+//         }
+//     )
+// });
